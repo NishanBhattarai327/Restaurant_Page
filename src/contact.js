c70de36b5afbd5f99ee619c189153ae0bb3253eb
NishanBhattarai327 <bhattarainishan327@gmail.com>
2021-06-30 10:10:57 +0545

    Add contact Info

--- src/contact.js
+++ src/contact.js
@@ -2,7 +2,21 @@ export default function contactTab() {
 	const contact = document.createElement('div');
 	contact.classList.add('contact');
 	
-	contact.innerHTML = "<h1>This is our Contact INfo.</h1>"
+	contact.appendChild(pTag(`
+		<h2><u>Contact Us</u></h2>
+		<br><br><br>
+		Phone Number: <span class='number'>9800011100</span><br>
+		Tel. Number: <span class='number'>025-7865</span><br>
+		Location: <span class='location'>Submit Of Sagarmatha, Nepal</span><br>
+		<br>
+	`));
 
 	return contact;
+}
+
+function pTag(text) {
+	const p = document.createElement('p');
+	p.classList.add('contact-info');
+	p.innerHTML = text;
+	return p;
 }
--- src/tab.js
+++ src/tab.js
@@ -50,7 +50,7 @@ function attachNavbarTo(body) {
 	navbar.appendChild(contact);
 
 	/*Default tab*/
-	renderTab(tabBody, aboutTab());
+	renderTab(tabBody, homeTab());
 
 	body.appendChild(navbar);
 	body.appendChild(tabBody);
